9494430f76428c39f7031c03974d6dff1d697cef
jeremias-2005 <1907791@clases.edu.sv>
2023-03-21 10:11:26 -0600

    agregar pagina de eleccion

---
+++ php/eleccion.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta name="description" content="Sitio web sobre control de notas INCAS">
+        <meta name="Keywords" content="Control de notas incas,CONTROL DE NOTAS INCAS,Control De Notas Incas,control de notas incas,incas,Incas,INCAS,&iacute;nstituto nacional cornelio azen&oacute;n Sierra, &Iacute;NSTITUTO NACIONAL CORNELIO AZEN&Oacute;N SIERRA,&Iacute;nstituto Nacional Cornelio Azen&oacute;n Sierra">
+        <meta name="author" content="Promoci&oacute;n de bachillerato tecnico vocacional en software año 2023">
+        <meta name="copyright" content="Sitio web sobre control de notas INCAS,Promoci&oacute;n de bachillerato tecnico vocacional en software año 2023">
+        
+        <!--Vinculaación de ficheros externos-->
+    <title>Consulta de notas INCAS</title>
+    <link rel="shourt icon" href="/control_de_notas/images/incas.png">
+    <link rel="stylesheet" type="text/css" href="/control_de_notas/css/principal.css" media="screen"/>
+    </head>
+    <body>
+       <h1 id="consulta">Consulta de notas</h1> 
+       <img id="decoracion" src="/control_de_notas/images/profesor.png">
+       <br><br><br><br><br><br><br><br>
+
+       <!--Formulario-->
+       <div class="formulario">
+            <h1 id="inicio">Elija la secci&oacute;n y grado deciado</h1>
+            <form method="post">
+                <div class="username">
+                    
+                </div>
+                <select class="seccion">
+                    <option disabled selected="">Selecciona una secci&oacute;n</option>
+                    <option>K</option>
+                    <option>O</option>
+                    <option>M</option>
+                    <option>L</option>
+                    <option>N</option>
+                    <option>D</option>
+                    <option>H</option>
+                    <option>G</option>
+                    <option>E</option>
+                    <option>B</option>
+                </select>
+                <select>
+                    <option disabled selected="">Selecciona un a&ntilde;o</option>
+                    <option>1</option>
+                    <option>2</option>
+                </select>
+                <div class="username">
+
+                </div>
+
+                <input type="submit" value="Aceptar">
+                <div class="Registrar"></div><br>
+                <input type="submit" value="Cancelar">
+                <div class="Registrar"></div>
+            </form>
+       </div>
+    </body>
+</html>
