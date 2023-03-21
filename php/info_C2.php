4bfa2ff1e4693bd78a0b4efd5a46ba9ea810066c
FernandoRodriguez12 <fernandojoseguinea.rodriguez@gmail.com>
2023-03-21 11:10:08 -0600

    change file

--- php/info_C2.php
+++ php/info_C2.php
@@ -1,8 +1,18 @@
 <?php
 include 'conexion.php';
 $con= conexion();
+//extraer datos desde la base para mostrar en pantalla
+$consulta4="SELECT nie, nombre_estudiante, act1 , act2 , po FROM tbl_notas INNER JOIN tbl_estudiante ON tbl_notas.nie = tbl_estudiantes.nie WHERE cod_periodo='1' AND cod_materia = '$Cmateria' AND cod_seccion = '$seccion' AND codigo_grado='$codGrado' ";
+$consulta5="SELECT  act1 , act2 , po FROM tbl_notas WHERE cod_periodo='2' AND cod_materia = '$Cmateria' AND cod_seccion='$seccion' AND codigo_grado='$codGrado'  ";
+$consulta6="SELECT  act1 , act2 , po FROM tbl_notas  WHERE cod_periodo='3' AND cod_materia = '$Cmateria' AND cod_seccion='$seccion' AND codigo_grado='$codGrado' ";
+$consulta7="SELECT  act1 , act2 , po FROM tbl_notas  WHERE cod_periodo='4' AND cod_materia = '$Cmateria' AND cod_seccion='$seccion' AND codigo_grado='$codGrado' ";
+$consulta8="SELECT re1, re2 , avanzo FROM tbl_notas  WHERE cod_materia = '$Cmateria' AND cod_seccion='$seccion' AND codigo_grado='$codGrado'  ";
 
-//extraer datos desde el cuadro 2
+
+
+
+
+//extraer datos desde el cuadro del segundo año
 $nie=$_POST['nie'];
 $act1=$_POST['act1'];
 $act2=$_POST['act2'];
@@ -21,20 +31,24 @@ if($select=="Periodo 1"){
 }
 
 
-//extraer datos de la prueba extraordinaria
+//extraer datos de la prueba extraordinaria del cuadro del segundo año
 if(isset($_POST['R_E1'])){
     $R_E1=$_POST['R_E1'];
     $consulta0="UPDATE tbl_notas SET re1='$R_E1' WHERE nie='$nie' AND cod_materia='$Cmateria'";
+    $query4=pg_query($con,$consulta0);
    
 }
 if(isset($_POST['R_E2'])){
     $R_E2=$_POST['R_E2'];
-    $consulta0="UPDATE tbl_notas SET re2='$R_E2' WHERE nie='$nie' AND cod_materia='$Cmateria'";
+    
+    $consulta1="UPDATE tbl_notas SET re2='$R_E2' WHERE nie='$nie' AND cod_materia='$Cmateria'";
+    $query5=pg_query($con,$consulta1);
     
 }
 if(isset($_POST['avanzo'])){
     $avanzo=$_POST['avanzo'];
-    $consulta0="UPDATE tbl_notas SET avanzo='$avanzo' WHERE nie='$nie' AND cod_materia='$Cmateria'";
+    $consulta2="UPDATE tbl_notas SET avanzo='$avanzo' WHERE nie='$nie' AND cod_materia='$Cmateria'";
+    $query6=pg_query($con,$consulta2);
    
 }
 
