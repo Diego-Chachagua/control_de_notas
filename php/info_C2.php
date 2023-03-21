d2c6cd5a4df20e3fdb617f860243741431cbee52
FernandoRodriguez12 <128391990+FernandoRodriguez12@users.noreply.github.com>
2023-03-21 07:38:29 -0600

    Add files via upload

---
+++ php/info_C2.php
@@ -0,0 +1,24 @@
+<?php
+include 'conexion.php';
+$con= conexion();
+
+//extraer datos desde el cuadro 2
+$nie=$_POST['nie'];
+$act1=$_POST['act1'];
+$act2=$_POST['act2'];
+$PO=$_POST['PO'];
+$select=$_POST['seleccion'];
+if($select=="Periodo 1"){
+    $option="Periodo1";
+}elseif($select=="Periodo 2"){
+    $option="Periodo2";   
+}elseif($select=="Periodo 3"){
+    $option="Periodo3";
+}elseif($select=="Periodo 4"){
+    $option="Periodo4";
+}
+
+//extraer datos de la prueba extraordinaria
+
+$P_E1=$_POST['R_E1'];
+$P_E1=$_POST['R_E2'];
