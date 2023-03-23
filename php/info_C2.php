d80b622fba9964a8ed0a186a22e7ba26fec74918
FernandoRodriguez12 <fernandojoseguinea.rodriguez@gmail.com>
2023-03-23 09:07:02 -0600

    actualizacion de cuadro 2 y info c 2

--- php/info_C2.php
+++ php/info_C2.php
@@ -2,9 +2,8 @@
 include 'conexion.php';
 $con= conexion();
 //extraer datos desde la base para mostrar en pantalla
-$consulta0="SELECT cod_nota, nombre_estudiante FROM tbl_notas INNER JOIN tbl_estudiantes ON tbl_notas.nie = tbl_estudiantes.nie ORDER BY cod_nota ASC";
-$query0=pg_query($con,$consulta0);
-$consulta="SELECT  act1 , act2 , po FROM tbl_notas  WHERE cod_periodo='1' ";
+
+$consulta="SELECT cod_nota, nombre_estudiante, act1 , act2 , po FROM tbl_notas  INNER JOIN tbl_estudiantes ON tbl_notas.nie=tbl_estudiantes.nie WHERE cod_periodo='1' ORDER BY cod_nota ASC";
 $query=pg_query($con,$consulta);
 $consulta1="SELECT act1 , act2 , po FROM tbl_notas WHERE cod_periodo='2' ";
 $query1=pg_query($con,$consulta1);
