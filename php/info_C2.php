7f34b8cdb5ce19302c787a9b6b1bd0fa7a94e8e8
FernandoRodriguez12 <fernandojoseguinea.rodriguez@gmail.com>
2023-03-23 10:53:24 -0600

    actualizacion de campos en tabla notas , file: notas.sql

--- php/info_C2.php
+++ php/info_C2.php
@@ -5,11 +5,11 @@ $con= conexion();
 
 $consulta="SELECT cod_nota, nombre_estudiante, act1 , act2 , po FROM tbl_notas  INNER JOIN tbl_estudiantes ON tbl_notas.nie=tbl_estudiantes.nie WHERE cod_periodo='1' ORDER BY cod_nota ASC";
 $query=pg_query($con,$consulta);
-$consulta1="SELECT act1 , act2 , po FROM tbl_notas WHERE cod_periodo='2' ";
+$consulta1="SELECT cod_nota, nombre_estudiante, act1 , act2 , po FROM tbl_notas  INNER JOIN tbl_estudiantes ON tbl_notas.nie=tbl_estudiantes.nie WHERE cod_periodo='2' ORDER BY cod_nota ASC ";
 $query1=pg_query($con,$consulta1);
 
-$consulta2="SELECT  act1 , act2 , po FROM tbl_notas WHERE cod_periodo='3' ";
+$consulta2="SELECT  cod_nota, nombre_estudiante, act1 , act2 , po FROM tbl_notas  INNER JOIN tbl_estudiantes ON tbl_notas.nie=tbl_estudiantes.nie WHERE cod_periodo='3' ORDER BY cod_nota ASC";
 $query2=pg_query($con,$consulta2);
 
-$consulta3="SELECT  act1 , act2 , po FROM tbl_notas  WHERE cod_periodo='4' ";
+$consulta3="SELECT cod_nota, nombre_estudiante, act1 , act2 , po FROM tbl_notas  INNER JOIN tbl_estudiantes ON tbl_notas.nie=tbl_estudiantes.nie WHERE cod_periodo='4' ORDER BY cod_nota ASC";
 $query3=pg_query($con,$consulta3);
