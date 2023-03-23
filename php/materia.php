e848198cbf29198735225e2d2ca2e4de698322d3
Diego-Chachagua <Diego-Chachagua>
2023-03-23 11:33:27 -0600

    Materia

--- php/materia.php
+++ php/materia.php
@@ -11,109 +11,109 @@
 <body>
     <h1 class="eleccion">ELIGA UNA MATERIA</h1>
     <h1  class="eleccion">¿QUE MATERIA DESEA ESCOGER?</h1>
-        <div class="materia">   
+        <button class="materia">   
             <div class="foto">
-                <img src="../images/quimica1.png" alt="">
+                <img class="ciencias" src="../images/quimica1.png" alt="">
             </div>
             <div class="pie">
                 <P class="letras">CIENCIAS</P>
             </div>
-        </div>
-   <div class="materia">
+        </button>
+   <button class="materia">
     <div class="foto">
       <img src="../images/sociales1.png" alt="">
       </div>
       <div class="pie">
           <p class="letras">SOCIALES</p>
       </div>
- </div>
- <div class="materia">
+</button>
+ <button class="materia">
     <div class="foto">
       <img src="../images/ingles1.png" alt="">
       </div>
       <div class="pie">
           <p class="letras">INGLES</p>
       </div>
- </div>
- <div class="materia">
+</button>
+ <button class="materia">
     <div class="foto">
       <img src="../images/oplv1.png" alt="">
       </div>
       <div class="pie">
           <p class="letras">OPLV</p>
       </div>
- </div>
- <div class="materia">
+</button>
+ <button class="materia">
     <div class="foto">
       <img src="../images/mate.png" alt="">
       </div>
       <div class="pie">
           <P class="letras">MATEMATICA</P>
       </div>
- </div>
- <div class="materia">
+</button>
+ <button class="materia">
     <div class="foto">
-      <img src="../images/lenguaje12.png" alt="">
+      <img src="../images/lenguaje1.png" alt="">
       </div>
       <div class="pie">
           <P class="letras">LENGUAJE</P>
       </div>
- </div>
- <div class="materia">
+</button>
+ <button class="materia">
     <div class="foto">
       <img src="../images/info3.png" alt="">
       </div>
       <div class="pie">
           <p class="letras">INFORMATICA</p>
       </div>
- </div>
- <div class="materia">
+</button>
+ <button class="materia">
     <div class="foto">
       <img src="../images/seminario1.png" alt="">
       </div>
       <div class="pie">
           <P class="letras">SEMINARIO</P>
       </div>
- </div>
- <div class="materia">
+</button>
+ <button class="materia">
     <div class="foto">
       <img src="../images/hpp1.png" alt="">
       </div>
       <div class="pie">
           <P class="letras">HPP</P>
       </div>
- </div>
- <div class="materia">
+</button>
+ <button class="materia">
     <div class="foto">
       <img src="../images/cultura12.png" alt="">
       </div>
       <div class="pie">
           <P class="letras">PAZ</P>
       </div>
- </div>
- <div class="materia">
+</button>
+ <button class="materia">
     <div class="foto">
       <img src="../images/decision1.png" alt="">
       </div>
       <div class="pie">
           <P class="letras">DECISION</P>
       </div>
- </div>
- <div class="materia">
+</button>
+ <button class="materia">
     <div class="foto">
       <img src="../images/participacion1.png" alt="">
       </div>
       <div class="pie">
           <p class="letras">PARTICIPACION</h2>
       </div>
- </div>
- <div class="materia">
+</button>
+ <button class="materia">
     <div class="foto">
       <img src="../images/cul.png" alt="">
       </div>
       <div class="pie">
           <P class="letras">CULTURA </P>
-      </div>
+</button>
  </div>
 </body>
 </html>
