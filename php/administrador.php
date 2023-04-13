c941cbd2f5499fa803c5358e021c89dbaf05ac90
FernandoRodriguez12 <fernandojoseguinea.rodriguez@gmail.com>
2023-04-13 07:47:05 -0600

    changes in two files , administrador y info_admin

--- php/administrador.php
+++ php/administrador.php
@@ -86,34 +86,34 @@
         <input type="submit" id="cancelar"  value="Cancelar"><br><br>
 </form>
     <h2 class="inst">Escriba los datos del profesor a registrar</h2><br>
-    <h3 class="materia2">Elija las materias[] que llevara el Profesor</h3><br>
-    <form action="info_admin" method="post">
+    <h3 class="materia2">Elija las materias que llevara el Profesor</h3><br>
+    <form action="info_admin.php" method="post">
         <div class="materias1" >
-    <input type="checkbox" name="materias[]" id="materias" class="check1"> Lenguaje
-    <input type="checkbox" name="materias[]" id="materias" class="check1"> Sociales
-    <input type="checkbox" name="materias[]" id="materias" class="check1"> Ciencia
-    <input type="checkbox" name="materias[]" id="materias" class="check1"> Muci
-    <input type="checkbox" name="materias[]" id="materias" class="check1"> Ingles
-    <input type="checkbox" name="materias[]" id="materias" class="check1"> Matem&aacute;tica
-    <input type="checkbox" name="materias[]" id="materias" class="check1"> Inform&aacute;tica
-    <input type="checkbox" name="materias[]" id="materias" class="check1"> Oplv
-    <input type="checkbox" name="materias[]" id="materias" class="check1"> Seminario
-    <input type="checkbox" name="materias[]" id="materias" class="check1"> Curso de habilitaci&oacute;n para la vida
+    <input type="checkbox" name="materias_prof[]" id="materias" class="check1" value="1"> Lenguaje
+    <input type="checkbox" name="materias_prof[]" id="materias" class="check1" value="4"> Sociales
+    <input type="checkbox" name="materias_prof[]" id="materias" class="check1" value="3"> Ciencia
+    <input type="checkbox" name="materias_prof[]" id="materias" class="check1" value="5"> Muci
+    <input type="checkbox" name="materias_prof[]" id="materias" class="check1" value="6"> Ingles
+    <input type="checkbox" name="materias_prof[]" id="materias" class="check1" value="2"> Matem&aacute;tica
+    <input type="checkbox" name="materias_prof[]" id="materias" class="check1" value="7"> Inform&aacute;tica
+    <input type="checkbox" name="materias_prof[]" id="materias" class="check1" value="8"> Oplv
+    <input type="checkbox" name="materias_prof[]" id="materias" class="check1" value="9"> Seminario
+    <input type="checkbox" name="materias_prof[]" id="materias" class="check1" value="10"> Curso de habilitaci&oacute;n para la vida
     </div><br>
     <div class="materias1">
-    <input type="checkbox" name="materias[]" id="materias" class="check1"> Evidencia actitudes favorables para la convivencia
-    <input type="checkbox" name="materias[]" id="materias" class="check1"> Toma desiciones de forma a&uacute;tonoma y responsable
-    <input type="checkbox" name="materias[]" id="materias" class="check1"> Se expresa y participa con respeto
+    <input type="checkbox" name="materias_prof[]" id="materias" class="check1" value="11"> Evidencia actitudes favorables para la convivencia
+    <input type="checkbox" name="materias_prof[]" id="materias" class="check1" value="12"> Toma desiciones de forma a&uacute;tonoma y responsable
+    <input type="checkbox" name="materias_prof[]" id="materias" class="check1" value="13"> Se expresa y participa con respeto
     </div><br>
     <div class="materias1">
-    <center><input type="checkbox" name="materias[]" id="materias" class="check1"> Muestra sentido de pertenencia y respeto por nuestra cultura</center>
+    <center><input type="checkbox" name="materias_prof[]" id="materias" class="check1" value="14"> Muestra sentido de pertenencia y respeto por nuestra cultura</center>
     </div><br>
     <div class="grid-layout">
         
                 <input type="text" id="nombre_alumno" name="nombre_profesor" placeholder="Nombre del profesor" >
        
        
-        <select>
+        <select name="grado_prof">
             <option disabled selected="">Selecciona un a&ntilde;o</option>
             <option>1</option>
             <option>2</option>
@@ -121,22 +121,18 @@
         </select>
        
     
-        <select class="seccion">
+        <select class="seccion" name="secciones_prof[]" multiple>
             <option disabled selected="">Selecciona una secci&oacute;n</option>
-            <option>K</option>
-            <option>O</option>
-            <option>M</option>
-            <option>L</option>
-            <option>N</option>
-            <option>D</option>
-            <option>H</option>
-            <option>G</option>
-            <option>E</option>
-            <option>B</option>
+            <option value="1">1<p>-A</p></option>
+            <option value="2">2<p>-F</p></option>
+            <option value="3">3<p>-E</p></option>
+            <option value="4">4<p>-H</p></option>
+            <option value="5">5<p>-G</p></option>
+            <option value="6">6<p>-D</p></option>
         </select>
        
-        <select>
-            <option disabled selected="">Selecciona un g&eacute;nero</option>
+        <select name="genero_prof">
+            <option disabled selected="" >Selecciona un g&eacute;nero</option>
             <option>Masculino</option>
             <option>Femenino</option>
         
@@ -149,18 +145,43 @@
         <input type="submit" id="cancelar"  value="Cancelar"><br><br>
 </form>
         <h2 class="inst">Escriba lo que desea eliminar</h2>
+       
         <div class="grid-layout2">
-            <form>
-                <select class="opcion">
+       
+            <form action="administrador.php" method="post">
+                <select class="opcion" name="eliminar">
                     <option disabled selected="">Selecciona un opci&oacute;n:</option>
                     <option>Maestro</option>
                     <option>Alumno</option>
                     <option>Padre</option>
                 </select>
-                <input type="text" id="eliminar" name="Eliminar" placeholder="Escriba lo que desea eliminar" >
+                <input type="text" id="eliminar" name="cod" placeholder="Escriba el Codigo/NIE/DUI que decea borrar" >
+<?php
+include 'conexion.php';
+$con=conexion();
+if(isset($_POST['eliminar'])){
+    if(isset($_POST['cod'])){
+        $codigo=$_POST['cod'];
+    }
+    $eliminar=$_POST['eliminar'];
+    if($eliminar=="Maestros"){
+       $consulta="DELETE FROM tbl_profesor WHERE cod_profe='$codigo'";
+       $query=pg_query($con,$consulta);
+    }elseif($eliminar=="Alumno"){
+        $consulta="DELETE FROM tbl_estudiantes WHERE nie='$codigo'";
+        $query=pg_query($con,$consulta);
+    }elseif($eliminar=="Padre"){
+     $consulta="DELETE FROM tbl_padres WHERE dui='$codigo'";
+    $query=pg_query($con,$consulta);
+    
+}
+}
+?>
+
         </div><br>
         <input type="submit" id="eliminar_total"  value="Eliminar"><br><br>
 </form>
+
 <form>
         <h2 class="inst">Ingrese de que a&ntilde;o y secci&oacute;n</h2><br>
         <div class="grid-layuot5">
--- php/info_admin.php
+++ php/info_admin.php
@@ -3,8 +3,6 @@ include 'conexion.php';
 $con=conexion();
 //entrada de datos para el registro de estudiantes
 $materias=$_POST['materias'];//contiene en array los codigos de las materias seleccionadas
-
-
 $nombre_estudiante=$_POST['nombre_alumno'];
 $grado=$_POST['grado'];
 $seccion=$_POST['seccion'];
@@ -22,6 +20,7 @@ if($nombre_estudiante=="" || $grado=="" || $seccion=="" || $genero=="" || $apell
     //insercion de datos en tabla padre
 $consulta5="INSERT INTO tbl_padres(dui,nombre_padre,apellido_padre) VALUES ('$dui_padre','$nombre_padre','$apellidos_padre')";
 $query5=pg_query($con,$consulta5);
+//comprobacion de la seccion seleccionada
     if($seccion=="A"){
         $cod_seccion="1";
     }elseif($seccion=="F"){
@@ -69,4 +68,90 @@ header("location: administrador.php");
 
 
 }
+
+//ingreso de datos para maestros
+$materias_prof=$_POST['materias_prof'];//contiene en array los codigos de las materias seleccionadas del profesor
+$grado_prof=$_POST['grado_prof'];
+$seccion_prof=$_POST['secciones_prof'];
+$genero_prof=$_POST['genero_prof'];
+$nombre_prof=$_POST['nombre_profesor'];
+
+$apellidos_prof=$_POST['apellidos_profesor'];
+if($grado_prof=="" || $seccion_prof=="" || $genero_prof=="" || $nombre_prof=="" || $apellidos_prof="" ){
+    //si cualquiera de los campos esta vacio
+    
+}else{
+//comprobacion de genero
+if($genero_prof=="Masculino"){
+    $genero_prof="M";
+}elseif($genero_prof=="Femenino"){
+    $genero_prof="F";
+}
+
+//consulta de usuario de bd para el ingreso de nuevo usuario
+$consulta7="SELECT usuario_profe, contrasena_profe FROM tbl_usuario";
+$query7=pg_query($con,$consulta7);
+//condicion para evaluar si un usuario ya esta repetido
+$x=1;
+while($x!=0){
+//generador de usuario y contraseña
+//generador de usuario con el nombre y un numero aleatorio
+$usuario=$nombre_prof.rand(1,100);
+//generador de contraseña
+$key = "";
+$length=6;
+$max=6;
+    for($i = 0; $i < $length; $i++){
+        $key .= substr($nombre_prof, mt_rand(0,$max), 1);
+    }
+    while($col=pg_fetch_Array($query7)){
+     if($usuario!=$col['usuario_profe'] && $key!=$col['contrasena_profe']){
+        $x=0;
+    }
+  }
+}
+//con un usuario y contraseña  se guarda el valor
+$consulta8="INSERT INTO tbl_usuario(usuario_profe,contrasena_profe) VALUES ('$usuario','$key')";
+$query8=pg_query($con,$consulta8);
+//ingreso de datos del maestro a la tabla profe_materia
+//obtener el ultimo codigo del profesor y sumarle +1 para asginar un nuevo codigo y llenar otras tablas
+$consulta9="SELECT cod_profe FROM tbl_profesor";
+$query9=pg_query($con,$consulta9);
+while($col1=pg_fetch_Array($query9)){
+    $cod_profe=$col1['cod_profe'];
+}
+$codigo_p=$cod_profe+1;
+//envio de datos a tabla profesor
+$consulta6="INSERT INTO tbl_profesor(nombre_profesor,apellido_profesor,genero) VALUES ('$nombre_prof','$apellidos_prof','$genero_prof')";
+$query6=pg_query($con,$consulta6);
+
+$contar_profe=count($materias_prof)-1;//contar la cantidad de elementos del array -1
+for($i=0;$i<=$contar_profe;$i++){
+    $consulta10="INSERT INTO tbl_profe_materia(cod_profe,cod_materia) VALUES ('$codigo_p','$materias_prof[$i]')";
+    $query10=pg_query($con,$consulta10);
+}
+//ingreso de datos del profesor a tabla profe_Secciones
+$contar_S=count($seccion_prof)-1;
+for($i=0;$i<=$contar_S;$i++){
+    $consulta11="INSERT INTO tbl_profe_seccion(cod_profe,cod_seccion) VALUES ('$cod_profe','$seccion_prof[$i]')";
+    $query11=pg_query($con,$consulta11);
+}
+//para llenar la tabla profe_grado
+if($grado_prof=="1"){
+    $consulta12="INSERT INTO tbl_profe_grado(cod_profe,cod_grado) VALUES ('$cod_profe','$grado_prof')";
+    $query12=pg_query($con,$consulta12);
+}elseif($grado_prof=="2"){
+    $consulta12="INSERT INTO tbl_profe_grado(cod_profe,cod_grado) VALUES ('$cod_profe','$grado_prof')";
+    $query12=pg_query($con,$consulta12);
+}elseif($grado_prof=="1 y 2"){
+    for($i=1;$i<=2;$i++){
+    $consulta12="INSERT INTO tbl_profe_grado(cod_profe,cod_grado) VALUES ('$cod_profe','$i')";
+    $query12=pg_query($con,$consulta12);
+    }
+}
+header("location: administrador.php");
+
+}//fin de funcionalidad de bloque para el ingreso de datos del profesor
+//oscar si ves esto el apartado de escoger seccion del profesor le hace falta mejoras en el css
+
 
